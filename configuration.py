4ef28748e9846f932f761fec8ff031e00092df9c
Natalia Molina <natalin.2920@gmail.com>
2025-05-12 19:15:12 +0930

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = 'https://cnt-8450f705-d3ce-42f9-9608-85374829cd6b.containerhub.tripleten-services.com'
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
